b387bf6be66573a43cabf6a467f51bba188efdbc
Khoirul Anam <khoirulanam4580@gmail.com>
2023-08-25 14:00:11 +0700

    update main page

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Foody - Organic Food Website Template</title>
+    <title>UMK MASAGENA</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -64,8 +64,8 @@
                 <div class="navbar-nav ms-auto p-4 p-lg-0">
                     <a href="index.html" class="nav-item nav-link active">Home</a>
                     <a href="#about" class="nav-item nav-link">Tentang</a>
-                    <a href="product.html" class="nav-item nav-link">Produk</a>
-                    <a href="contact.html" class="nav-item nav-link">Kontak</a>
+                    <a href="#produk" class="nav-item nav-link">Produk</a>
+                    <a href="#kontak" class="nav-item nav-link">Kontak</a>
                 </div>
                 <div class="d-none d-lg-flex ms-2">
                     <a class="btn-sm-square bg-white rounded-circle ms-3" href="/login">
@@ -118,7 +118,7 @@
                     <p class="mb-4">UMK Masagena telah berdiri sejak tahun 2000 dengan pemilik bernama Andi
                         Muqtadirah, bergerak dalam pengolahan nanas
                         menjadi berbagai produk makanan seperti aneka kue dan selai. UMK Masagena berlokasi di Desa
-                        Tangkit Baru, Kecamatan SUngai Gelam, Kabupaten Muaro Jambi, Jambi.</p>
+                        Tangkit Baru, Kecamatan Sungai Gelam, Kabupaten Muaro Jambi, Jambi.</p>
                     <p><i class="fa fa-check text-primary me-3"></i>Menggunakan Nanas Berkualitas</p>
                     <p><i class="fa fa-check text-primary me-3"></i>Menjadi oleh-oleh yang selalu dicari</p>
                     <p><i class="fa fa-check text-primary me-3"></i>Aneka produk olahan nanas yang sehat dan enak</p>
@@ -129,7 +129,7 @@
     <!-- About End -->
 
     <!-- Product Start -->
-    <div class="container-xxl py-5">
+    <div class="container-xxl py-5" id="produk">
         <div class="container">
             <div class="row g-0 gx-5 align-items-end">
                 <div class="col-lg-6">
@@ -178,17 +178,18 @@
 
 
     <!-- Firm Visit Start -->
-    <div class="container-fluid bg-primary bg-icon mt-5 py-6">
+    <div class="container-fluid bg-primary bg-icon mt-5 py-6" id="kontak">
         <div class="container">
             <div class="row g-5 align-items-center">
                 <div class="col-md-7 wow fadeIn" data-wow-delay="0.1s">
-                    <h1 class="display-5 text-white mb-3">Visit Our Firm</h1>
-                    <p class="text-white mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam
-                        amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna
-                        dolore erat amet. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos.</p>
+                    <h1 class="display-5 text-white mb-3">Kunjungi Kami</h1>
+                    <p class="text-white mb-0">Anda dapat mengunjungi kami di di Desa
+                        Tangkit Baru, Kecamatan Sungai Gelam, Kabupaten Muaro Jambi, Jambi. Atau dapat melakukan
+                        pemesanan melalui nomor berikut : 08</p>
                 </div>
                 <div class="col-md-5 text-md-end wow fadeIn" data-wow-delay="0.5s">
-                    <a class="btn btn-lg btn-secondary rounded-pill py-3 px-5" href="">Visit Now</a>
+                    <a class="btn btn-lg btn-secondary rounded-pill py-3 px-5" href="#kontak">Pesan oleh-oleh Berbahan
+                        Nanas terbaik dari Jambi</a>
                 </div>
             </div>
         </div>
@@ -200,19 +201,9 @@
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
-                    <h1 class="fw-bold text-primary mb-4">F<span class="text-secondary">oo</span>dy</h1>
+                    <h1 class="fw-bold text-primary mb-4"><span class="text-secondary">MASAGENA</span></h1>
                     <p>Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed
                         stet lorem sit clita</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i
-                                class="fab fa-twitter"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i
-                                class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i
-                                class="fab fa-youtube"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-0" href=""><i
-                                class="fab fa-linkedin-in"></i></a>
-                    </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Address</h4>
@@ -220,31 +211,13 @@
                     <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
                     <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
                 </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Quick Links</h4>
-                    <a class="btn btn-link" href="">About Us</a>
-                    <a class="btn btn-link" href="">Contact Us</a>
-                    <a class="btn btn-link" href="">Our Services</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">Support</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Newsletter</h4>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                        <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text"
-                            placeholder="Your email">
-                        <button type="button"
-                            class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                    </div>
-                </div>
             </div>
         </div>
         <div class="container-fluid copyright">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a href="#">Your Site Name</a>, All Right Reserved.
+                        &copy; <a href="#">UMK MASAGENA</a>, All Right Reserved.
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
